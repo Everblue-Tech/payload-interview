ee5b4b545e0cd3f60c3981ad29f03b2e2edd4082
Jacob Barnett <2818246+jacoblb64@users.noreply.github.com>
2025-09-24 17:22:56 -0400

    feat: saying hi
    - added before dashboard component
    - said hi on the base page and dashboard page

---
+++ src/components/BeforeDashboard.tsx
@@ -0,0 +1,11 @@
+const baseClass = 'before-dashboard'
+
+const BeforeDashboard: React.FC = () => {
+  return (
+    <div className={baseClass}>
+      <span>Hello from Jacob!</span>
+    </div>
+  )
+}
+
+export default BeforeDashboard

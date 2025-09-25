837a511e90c0d95efa4412ddcdede68574c620a3
igloude <igloude@gmail.com>
2025-09-25 10:58:18 -0500

    whoops, forgot importmap

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -25,9 +25,9 @@ export default async function HomePage() {
     <div className="flex flex-col h-screen p-4">
       <div className="lg:flex lg:items-center lg:justify-between">
         <div className="min-w-0 flex-1">
-          <h1 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight dark:text-white">
+          <h2 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight dark:text-white">
             Taskr
-          </h1>
+          </h2>
           <p className="text-lg">Because every good app name ends in an r</p>
         </div>
         <div className="mt-5 flex lg:mt-0 lg:ml-4 gap-2">
